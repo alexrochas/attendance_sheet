79a185328e486748aeac71d7804efbcae67988c3
Alex Rocha <alex.rochas@yahoo.com.br>
2018-04-22 21:08:18 -0300

    fix: home page to sheet url

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'attendance/sheet'
   post 'attendance/register'
 
+  root 'attendance#sheet'
+
   resources :students
   resources :presences
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
